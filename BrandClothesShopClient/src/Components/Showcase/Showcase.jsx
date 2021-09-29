d32c8aab4ca7b16347405fcfb42612a4e41a4f90
wh4y <wh4y@mail.ru>
2021-09-29 17:19:32 +0300

    Showcase component was created

---
+++ BrandClothesShopClient/src/Components/Showcase/Showcase.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import 'antd/dist/antd.css';
+
+const Showcase = (props) => {
+    return (
+        <>
+            SHOWCASE
+        </>
+    );
+}
+
+export default Showcase;
