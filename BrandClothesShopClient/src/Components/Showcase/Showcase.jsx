ddfb40f77d504121dbe161656527f294d782a03e
wh4y <wh4y@mail.ru>
2021-09-29 17:54:21 +0300

    Grid layout was customized

--- BrandClothesShopClient/src/Components/Showcase/Showcase.jsx
+++ BrandClothesShopClient/src/Components/Showcase/Showcase.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import 'antd/dist/antd.css';
+import styles from './style/styles.module.scss';
 
 const Showcase = (props) => {
     return (
         <>
-            SHOWCASE
+            <div className={styles.wrapper}>
+                <div className={styles.item}>I T E M</div>
+                <div className={styles.item}>I T E M</div>
+                <div className={styles.item}>I T E M</div>
+                <div className={styles.item}>I T E M</div>
+                <div className={styles.item}>I T E M</div>
+                <div className={styles.item}>I T E M</div>
+                <div className={styles.item}>I T E M</div>
+                <div className={styles.item}>I T E M</div>
+            </div>
         </>
     );
 }
