b0d0e75fc6384252b039690554195d5b9990cf3e
moltenessence <whatdoessence@gmail.com>
2021-10-06 23:47:03 +0300

    Adding ICart Interface

---
+++ BrandClothesShopAPI/BrandClothesShopAPI/Controllers/CartController.cs
@@ -0,0 +1,39 @@
+﻿using BrandClothesShopAPI.Models;
+using DataStore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrandClothesShopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartController : ControllerBase, ICartController
+    {
+        private readonly ClothesShopContext _context;
+        public CartController(ClothesShopContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> AddItemIntoCart(int id)
+        {
+
+        }
+
+        public Task<ActionResult> DeleteItemFromCart(int id)
+        {
+            
+        }
+
+        public Task<ActionResult> GetAllItemsFromCart()
+        {
+           
+        }
+    }
+}
---
+++ BrandClothesShopAPI/BrandClothesShopAPI/Controllers/ICartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrandClothesShopAPI.Controllers
+{
+    interface ICartController
+    {
+        Task<ActionResult> AddItemIntoCart(int id);
+        Task<ActionResult> DeleteItemFromCart(int id);
+        Task<ActionResult> GetAllItemsFromCart();
+    }
+}
