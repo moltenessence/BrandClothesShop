c3985c96188299abddb4a351e1144ee49fe203ab
moltenessence <whatdoessence@gmail.com>
2021-10-23 17:20:10 +0300

    Deleting the limit of items can be token

--- BrandClothesShopAPI/BrandClothesShopAPI/Controllers/ItemsController.cs
+++ BrandClothesShopAPI/BrandClothesShopAPI/Controllers/ItemsController.cs
@@ -14,7 +14,7 @@ namespace BrandClothesShopAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ItemsController : ControllerBase, IItemsController
+    public class ItemsController : ControllerBase
     {
         private readonly ClothesShopContext _context;
         public ItemsController(ClothesShopContext context)
@@ -31,8 +31,8 @@ namespace BrandClothesShopAPI.Controllers
             var totalAmount = _context.ClothesItems.Where(i => i.Type == type).Count();
             var amountToSkip = page == 1 ? 0 : (page - 1) * count;
 
-            if (count * page > totalAmount)
-                return BadRequest($"The number of items to take is out of range! Total amount of items of type '{type}' = {totalAmount}");
+            //if (count * page > totalAmount)
+            //    return BadRequest($"The number of items to take is out of range! Total amount of items of type '{type}' = {totalAmount}");
 
             var clothesItems = await _context.ClothesItems.Where(i => i.Type == type)
                                                           .Include("Photos")
