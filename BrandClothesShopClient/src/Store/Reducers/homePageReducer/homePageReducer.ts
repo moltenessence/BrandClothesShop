fcd86220a582fc3d376af2445c6c1cba64398cf2
wh4y <wh4y@mail.ru>
2021-10-25 15:24:41 +0300

    homePageReducer migration to ts : initial state

--- BrandClothesShopClient/src/Store/Reducers/homePageReducer/homePageReducer.ts
+++ BrandClothesShopClient/src/Store/Reducers/homePageReducer/homePageReducer.ts
@@ -1,10 +1,16 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { setBackground } from './actionCreators';
 
+interface IHomePageState {
+    backgroundUrl: string,
+}
+
+const intialState: IHomePageState = {
+    backgroundUrl: '',
+}
+
 const homePage = createReducer(
-    {
-        backgroundUrl: '',
-    },
+    intialState,
     {
         [setBackground.SUCCESS]: (state, action) => {
             state.backgroundUrl = action.payload.url;
