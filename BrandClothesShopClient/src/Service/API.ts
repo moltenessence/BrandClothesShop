5696068d714dd892fbf703bff8d3ee21a9006581
wh4y <wh4y@mail.ru>
2021-11-30 01:12:11 +0300

    API refactoring

--- BrandClothesShopClient/src/Service/API.ts
+++ BrandClothesShopClient/src/Service/API.ts
@@ -2,12 +2,14 @@ import axios from "axios";
 
 const baseURL = 'http://localhost:60671/api';
 
+const baseInstanceObject = {
+    baseURL,
+}
+
 //--------------
 //AuthMe API
 //--------------
-export const AuthMeAPI = axios.create({
-    baseURL,
-});
+export const AuthMeAPI = axios.create(baseInstanceObject);
 
 AuthMeAPI.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
@@ -17,23 +19,17 @@ AuthMeAPI.interceptors.request.use(config => {
 //--------------
 //Showcase API
 //--------------
-export const ShowcaseAPI = axios.create({
-    baseURL,
-});
+export const ShowcaseAPI = axios.create(baseInstanceObject);
 
 //--------------
 //Showcase API
 //--------------
-export const BackgroundAPI = axios.create({
-    baseURL,
-});
+export const BackgroundAPI = axios.create(baseInstanceObject);
 
 //--------------
 //Order API
 //--------------
-export const OrderAPI = axios.create({
-    baseURL,
-});
+export const OrderAPI = axios.create(baseInstanceObject);
 
 OrderAPI.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
