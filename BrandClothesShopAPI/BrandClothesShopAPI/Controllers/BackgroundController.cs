a1113937f037d0816dcc29065f7cfc4dae1a77a4
moltenessence <whatdoessence@gmail.com>
2021-12-14 15:33:21 +0300

    Update BackgroundController.cs

--- BrandClothesShopAPI/BrandClothesShopAPI/Controllers/BackgroundController.cs
+++ BrandClothesShopAPI/BrandClothesShopAPI/Controllers/BackgroundController.cs
@@ -9,6 +9,6 @@ namespace BrandClothesShopAPI.Controllers
         private const string homepageUrl = "https://sun9-85.userapi.com/impg/2HS1bcEIwKWLNh113oVCMc1wd7qSGiCb58qN3w/4W97P3gZzao.jpg?size=959x1280&quality=96&sign=c5daa24b22e0b09f99e550d63feb663a&type=album";
 
         [HttpGet("Homepage")]
-        public ActionResult GetHomepageImageUrl() => new OkObjectResult(homepageUrl);
+        public ActionResult GetHomepageImageUrl() => new OkObjectResult(new { url = homepageUrl});
     }
 }
